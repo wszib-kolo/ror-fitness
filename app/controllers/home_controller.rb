ab8c6e20db1ca2fdb97a65dfdd30988c7ae443d0
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-03-08 20:28:21 +0100

    Showing 3 upcoming classes on the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-	@schedule = Schedule.all
+	@schedule = Schedule.where("schedules.student_id = ?", current_user.id).order(date: :asc).take(3)
+
   end
 end
