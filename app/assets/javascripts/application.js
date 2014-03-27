5a631e4ca84e52d7a4efcac6405c339d78cc0209
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-03-27 16:23:52 +0100

    jquery need to be loaded before bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap
 //= require dataTables/jquery.dataTables
 //= require dataTables/jquery.dataTables.bootstrap3
 //= require_tree .
