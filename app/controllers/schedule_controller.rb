689d4548588c9e00c6c9b32458d0f65288b4940b
Kacper3331 <denebv201@gmail.com>
2014-04-03 11:08:44 +0200

    added amount collumn to schedules

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -2,6 +2,17 @@ class ScheduleController < ApplicationController
   def schedules
   	@schedule = current_user.schedules.order(id: :asc, date: :asc)
 
-  	@reserv = SchedulesUsers.distinct.count('user_id')
+  	@user_id = Schedule.select('schedule_id')
+
+  	@user = SchedulesUsers.select('schedule_id')
+
+  	if @user_id = @user
+ 
+  	@reserv = SchedulesUsers.count('schedules_users.schedule_id')
+  
+  	end
+
+  	@id = Schedule.where(:id => params[:id])
+  	schedules = SchedulesUsers.create(:user_id => current_user.id, :schedule_id => @id )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Fitness::Application.routes.draw do
   get "schedule/schedules"
   get "home/index"
-  #get "home/test"
+  
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root to: 'home#index'
+
+  resources :schedules do 
+   get "schedules", :on => :collection
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
